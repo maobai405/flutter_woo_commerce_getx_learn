d6125ab76ba2ed0b46d6813129a29a72caddf866
maobai405 <maobai405@gmail.com>
2024-04-26 13:43:06 +0800

    ScreenUtil 屏幕适配

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'common/index.dart';
@@ -15,28 +16,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: const MaterialTheme(TextTheme()).light(),
-      darkTheme: const MaterialTheme(TextTheme()).dark(),
+    return ScreenUtilInit(
+      designSize: const Size(414, 896), // 设计稿中设备的尺寸(单位随意,建议dp,但在使用过程中必须保持一致)
+      // splitScreenMode: false, // 支持分屏尺寸
+      // minTextAdapt: false, // 是否根据宽度/高度中的最小值适配文字
+      // 一般返回一个MaterialApp类型的Function()
+      builder: (context, child) {
+        return GetMaterialApp(
+          title: 'Flutter Demo',
 
-      // 主题
-      themeMode:
-          ConfigService.to.isDarkModel ? ThemeMode.dark : ThemeMode.light,
+          // 样式
+          theme: ConfigService.to.isDarkModel
+              ? const MaterialTheme(TextTheme()).dark()
+              : const MaterialTheme(TextTheme()).light(),
 
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          // 路由
+          initialRoute: RouteNames.stylesStylesIndex,
+          getPages: RoutePages.list,
+          navigatorObservers: [RoutePages.observer],
 
-      // 路由
-      initialRoute: RouteNames.stylesStylesIndex,
-      getPages: RoutePages.list,
-      navigatorObservers: [RoutePages.observer],
+          // builder
+          builder: (context, widget) {
+            // 不随系统字体缩放比例
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: widget!,
+            );
+          },
 
-      // 多语言
-      translations: Translation(), // 词典
-      localizationsDelegates: Translation.localizationsDelegates, // 代理
-      supportedLocales: Translation.supportedLocales, // 支持的语言种类
-      locale: ConfigService.to.locale, // 当前语言种类
-      fallbackLocale: Translation.fallbackLocale, // 默认语言种类
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
