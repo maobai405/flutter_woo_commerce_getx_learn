a82cebc531938122ed9f5f9fed3a810c1cee530f
maobai405 <maobai405@gmail.com>
2024-04-26 15:53:54 +0800

    WelcomeSlider 封装

--- lib/pages/system/splash/controller.dart
+++ lib/pages/system/splash/controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_woo_commerce_getx_learn/common/index.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
@@ -8,6 +9,14 @@ class SplashController extends GetxController {
     // 删除设备启动图
     FlutterNativeSplash.remove();
     update(["splash"]);
+  }
+
+  _jumpToPage() {
+    // 欢迎页
+    Future.delayed(const Duration(seconds: 1), () {
+      /// offAllNamed 会删除所有的路由然后跳转到指定的路由
+      Get.offAllNamed(RouteNames.systemWelcome);
+    });
   }
 
   void onTap() {}
@@ -21,6 +30,7 @@ class SplashController extends GetxController {
   void onReady() {
     super.onReady();
     _initData();
+    _jumpToPage();
   }
 
   // @override
